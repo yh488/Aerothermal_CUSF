a5cd46e6013f572b59cd7454f0b4e915e7dfdd3d
yh488 <yh488@cam.ac.uk>
2023-10-08 16:51:22 +0100

    setup pip

---
+++ Project File.py
@@ -0,0 +1,8 @@
+# pip install numpy
+# pip install numpy --upgrade --ignore-installed
+# pip install wheel
+# pip install --upgrade pip
+# pip install --upgrade setuptools
+# pip install thermo
+# pip install gas_dynamics
+# pip install ambiance
